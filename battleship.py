2c55a6cd0c2fb0cd51751d207b59dd3d5cffef47
Spina02 <47366784+Spina02@users.noreply.github.com>
2021-10-25 01:11:53 +0200

    Update battleship.py

--- battleship.py
+++ battleship.py
@@ -1,11 +1,17 @@
 import numpy as np                          #libreria per le matrici
-from random import choice, randint
+from random import randint
+import os
+import sys
+
+def wcls():
+    os.system("cls")
+    return
 
 def grid_fun():                             #crea una matrice 8x8 di zeri
     grid = np.full((8, 8), '0')             #corrisponde su python ad una lista di liste
     return grid
 
-def check(boat):                            #controlla se le caselle dove devono andare le barche sono vuote o occupate
+def check(grid, boat, src1, src2, x, dir, usr_cpu):             #controlla se le caselle dove devono andare le barche sono vuote o occupate
     count = 0                               #variabile counter
     for c in boat:
         if c == '0':                        #se l'elemento è uno zero allora indica una casella vuota...
@@ -13,8 +19,25 @@ def check(boat):                            #controlla se le caselle dove devono
     if count == len(boat):                  #se il counter segna un valore pari a quello della barca...
         return True                         #...allora tutte e x le caselle sono vuote, ritorna vero
     else:
-        return False                        #altrimenti ritorna falso
-
+        if usr_cpu == "cpu":
+            src1 = randint(0, 7)                #altrimenti ritorna falso
+            src2 = randint(0, 7 - x)
+            if dir:
+                boat = grid[src1:src1 + 1, src2:src2 + x]
+                check(grid, boat, src1, src2, x, dir, usr_cpu)
+            else:
+                boat = grid[src1:src1 + x, src2:src2 + 1]
+                check(grid, boat, src1, src2, x, dir, usr_cpu)
+        else:
+            print('Coordinates invalid. Try again')
+            src1 = int(input('Inserisci la riga: '))
+            src2 = int(input('Inserisci la colonna: '))
+            if dir:
+                boat = grid[src1:src1 + 1, src2:src2 + x]
+                check(grid, boat, src1, src2, x, dir, usr_cpu)
+            else:
+                boat = grid[src1:src1 + x, src2:src2 + 1]
+                check(grid, boat, src1, src2, x, dir, usr_cpu)
 
 def coll(grid):                                                    #colloca le barche della cpu
     for x in boats:                                                 #boats -> vedi "variabili", il valore x indica anche la lunghezza della barca
@@ -24,21 +47,37 @@ def coll(grid):                                                    #colloca le b
         if dir:                                                     #(= se orizzontale)
             column = randint(0, 7 - x)                                    #sceglie la colonna in cui si trova l'estremo sinistro della barca
             boat = grid[line:line + 1, column:column + x]           #crea una sottomatrice con le sole celle di dove si troverà la barca
-            while not check(boat[0]):                               #finchè le non è rispettato il check:
-                line = randint(0, 7)                                      #genera randomicamente altri punti...
-                column = randint(0, 7 - x)                                #...di collocazione per la barca
-                boat = grid[line:line + 1, column:column + x]       #e li assegna alla barca
+            check(grid, boat[0], line, column, x, dir, "cpu")              #controlla che le caselle siano vuote
             for i in range(x):                                      #sostituisce gli zeri nella matrice con gli 1...
                 grid[line, column+i] = '1'                          #...nei punti in cui è presente una barca
         else:                                                       #analogamente se la direzione è verticale
             line = randint(0, 7 - x)
             boat = grid[line:line + x, column:column + 1]
-            while not check(boat):
-                column = randint(0, 7)
-                line = randint(0, 7 - x)
-                boat = grid[line:line + x, column:column + 1]
+            check(grid, boat[0], column, line, x, dir, "cpu")
             for ii in range(x):
                 grid[line + ii, column] = '1'
+
+def coll_user(grid):
+    for x in boats:
+        print(f"la barca che stai piazzando è da {x} spazi")
+        line = int(input('Inserisci la riga: '))
+        column = int(input('Inserisci la colonna: '))
+        dir = input('Choose the direction (h or v): ')
+        if dir.lower == 'horizontal' or dir == 'h':
+            boat = grid[line: line + 1, column:column + x]
+            check(grid, boat[0], line, column, x, dir, "usr")
+            for i in range(x):
+                grid[line-1, column + i-1] = '1'
+            show_grid(grid_usr)
+        elif dir.lower == 'vertical' or dir == 'v':
+            boat = grid[line: line + x, column:column + 1]
+            check(grid, boat[0], column, line, x, dir, "usr")
+            for ii in range(x):
+                grid[line + ii -1, column-1] = '1'
+            show_grid(grid_usr)
+        else:
+            print("Wrong input. Try again")
+            pass
 
 
 def show_grid(grid):                                                        #crea una rappresentazione grafica per la matrice
@@ -60,15 +99,16 @@ def show_grid(grid):                                                        #cre
 
 
 #variabili
-boats = [4, 3, 3, 2, 2, 2, 1, 1, 1]                   #ogni elemento è una barca di lunghezza pari al suo valore
-grid_1 = grid_fun()                      #crea la variabile a cui è associata la matrice
-grid_2 = grid_fun()
+boats = [4, 3, 2, 1]                       #ogni elemento è una barca di lunghezza pari al suo valore
+grid_cpu = grid_fun()                      #crea la variabile a cui è associata la matrice
+grid_usr = grid_fun()
 
 def main():
-    coll(grid_1)                        #colloca le barche della cpu
-    coll(grid_2)
-    show_grid(grid_1)                    #stampa il campo da gioco
-    show_grid(grid_2)
+    coll(grid_cpu)     #colloca le barche della cpu
+    show_grid(grid_usr)
+    coll_user(grid_usr)
+    show_grid(grid_cpu)                    #stampa il campo da gioco
+    show_grid(grid_usr)
 
 if __name__ == '__main__':
     main()
